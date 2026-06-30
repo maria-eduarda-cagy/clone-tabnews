66bbbdaa10fc94885afcf83596cfbaa386d7587b
Maria Cagy <mariacagy@votingbuddy.com>
2026-06-29 22:45:56 -0300

    feat: integrate PostgreSQL with API status endpoint and update dependencies
    - Added `pg` dependency for PostgreSQL support.
    - Updated the API status endpoint to query the database.
    - Modified Docker Compose configuration to use an environment file for database credentials.
    - Updated test script to watch all tests.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,10 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+    const result = await database.query("SELECT 1 + 1;");
+    console.log(result);
 res.status(200).json({ status: 'ok' })
+
 }
 
 export default status;
